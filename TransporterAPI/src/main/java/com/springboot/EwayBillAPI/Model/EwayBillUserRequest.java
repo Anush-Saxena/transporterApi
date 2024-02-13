d13d13a3549b4e5d03ee19061850c8d262c6b676
jayshree2419 <jayshreechoudhary15@gmail.com>
2024-02-13 19:59:07 +0530

    Documentation code updated

--- TransporterAPI/src/main/java/com/springboot/EwayBillAPI/Model/EwayBillUserRequest.java
+++ TransporterAPI/src/main/java/com/springboot/EwayBillAPI/Model/EwayBillUserRequest.java
@@ -1,12 +1,18 @@
 package com.springboot.EwayBillAPI.Model;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class EwayBillUserRequest {
-    private String username;
+	@NotBlank
+	private String username;
+	@NotBlank
     private String password;
+	@NotBlank
     private String gstin;
+	@NotBlank
     private String role;
     private long stateCode;
 
--- TransporterAPI/src/main/java/com/springboot/SpringFoxWebConfiguraton.java
+++ TransporterAPI/src/main/java/com/springboot/SpringFoxWebConfiguraton.java
@@ -1,24 +1,45 @@
 package com.springboot;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxWebConfiguraton {
 
+public static final String AUTHORIZATION_HEADER="Authorization";
+	
 
     @Bean
     public Docket apiv1() {
         return new Docket(DocumentationType.SWAGGER_2)
         		.groupName("api-v1")
+        		.apiInfo(getInfo())
         		.select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+    
+    private ApiInfo getInfo()
+    {
+		return new ApiInfo("TRANSPORTER API DOCUMENTATION","This documentaion gives the detailed explanation of all the services under Transporter Api","1.0","Terms of Service",new Contact("Liveasy","Liveasy Website","liveasy mailId"),"License Of Api","LicenSE Url", Collections.emptyList());
+    	 
     }
 }
